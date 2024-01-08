afb1ac906209ad880ac7dc0c651c16800b3bda19
Graciella05 <degboegraciella@gmail.com>
2024-01-08 12:02:51 +0000

    modification

--- index.php
+++ index.php
@@ -1,44 +1,7 @@
 <?php
+
     session_start();
-
-    require_once('Database/connect.php');
-    require_once('Model/Eleve.php');
-    require_once('Model/Note.php');
-
-    $nombre = $_SESSION['nombre'];
-
-        if(isset($_POST['validate'])){
-            if(!empty($_POST['nom']) and !empty($_POST['prenom']) and !empty($_POST['note1']) and !empty($_POST['note2'])){
-
-                //récupérer les valeurs du formulaire
-                $nom = htmlspecialchars($_POST['nom']);
-                $prenom = htmlspecialchars($_POST['prenom']);
-                $note1 = htmlspecialchars($_POST['note1']);
-                $note2 = htmlspecialchars($_POST['note2']);
-
-                $eleve = new Eleve();
-                $note = new Note();
-        
-                // Exemple d'utilisation : ajouter un élève et une note
-                $eleve->ajouterEleve($nom, $prenom);
-        
-                 $eleves = $eleve->getEleves(); 
-                // Supposons que le premier élève a été ajouté
-                            $eleveId = $eleves[0]['id'];
-                        $note->ajouterNote($eleveId, $note1, $note2);
-          
-            } else{
-                echo 'Veuillez renseigner tout les champs !!';
-            }
-
-           
-            // Afficher la liste des élèves
-            /*   {
-                $eleveId = $eleves[0]['id'];
-                echo $e['prenom'] . ' ' . $e['nom'] . $e['id'] .'<br>';
-            }  */
-
-        }
+    require_once('Controller/Registercontrol.php');
 
 ?>
 <!DOCTYPE html>
@@ -49,17 +12,10 @@
     <title>Enregistrement de notes</title>
     <link rel="stylesheet" href="style.css">
 </head>
-<body>
+<body> <br> <br>
 
-    <form action="" method="POST">
-        <p>Entrer le nom de l'élève :  <input type="text" name="nom" id=""></p>
-        <p>Entrer le prénom de l'élève :  <input type="text" name="prenom" id=""></p>
-        <p>Entrer la première note de l'élève : <input type="number" name="note1" id=""></p>
-        <p>Entrer la deuxième note de l'élève : <input type="number" name="note2" id=""></p>
-        <p><button type="submit" name='validate' >Envoyer</button></p>
-    </form>
-    
-
-    
+    <p><center><strong>Enregistrement des notes des élèves</strong></center></p> <br> <br> <br>
+    <center><button type="submit" name="envoyer">Enregistrer les notes</button></center>                                  
+   
 </body>
 </html>
