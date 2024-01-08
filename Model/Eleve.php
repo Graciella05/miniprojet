d923d3b6a10c4dacbd85a68a77babbc0ed31a0ff
Graciella05 <degboegraciella@gmail.com>
2024-01-08 10:47:48 +0000

    second commit

---
+++ Model/Eleve.php
@@ -0,0 +1,29 @@
+<?php
+    require_once('Database/connect.php');
+    class Eleve {
+        private $db;
+        public $id;
+        public $nom;
+        public $prenom;
+    
+        public function __construct() {
+            $this->db = new Database();
+        }
+    
+        public function ajouterEleve($nom, $prenom) {
+            $query = 'INSERT INTO eleve (nom, prenom) VALUES (:nom, :prenom)';
+            $stmt = $this->db->conn->prepare($query);
+            $stmt->bindParam(':nom', $nom);
+            $stmt->bindParam(':prenom', $prenom);
+            return $stmt->execute();
+        }
+    
+        public function getEleves() {
+            $query = 'SELECT * FROM eleve';
+            $stmt = $this->db->conn->prepare($query);
+            $stmt->execute();
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        }
+    }
+
+?>
---
+++ Model/Note.php
@@ -0,0 +1,22 @@
+<?php
+    require_once('Database/connect.php');
+    class Note extends Database {
+        public function ajouterNote($eleveId, $note1, $note2) {
+            $query = 'INSERT INTO note (eleve_id, note1, note2) VALUES (:eleve_id, :note1, :note2)';
+            $stmt = $this->conn->prepare($query);
+            $stmt->bindParam(':eleve_id', $eleveId);
+            $stmt->bindParam(':note1', $note1);
+            $stmt->bindParam(':note2', $note2);
+            return $stmt->execute();
+        }
+    
+        public function getNotes() {
+            $query = 'SELECT * FROM note ' ;
+            $AllNotes = $this->conn->prepare($query);
+            $AllNotes->execute();
+            return $AllNotes->fetchAll();
+        }
+    }
+
+   
+?>
